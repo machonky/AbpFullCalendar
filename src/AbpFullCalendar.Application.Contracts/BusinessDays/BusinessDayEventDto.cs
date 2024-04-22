8803fb472be8818cd7f2fb41bad3931728a62ba7
machonky <martin@macrobert.com>
2024-04-22 16:58:42 +0800

    Create BusinessDayEventDto.cs

---
+++ src/AbpFullCalendar.Application.Contracts/BusinessDays/BusinessDayEventDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpFullCalendar.BusinessDays;
+
+public class BusinessDayEventDto
+{
+    public string id { get; set; }
+    public string title { get; set; }
+    public string start { get; set; }
+    public bool allDay { get; set; } = true;
+}

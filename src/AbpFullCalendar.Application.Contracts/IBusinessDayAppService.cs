bfb35fb20ec9fe5887f230177ed9c11903693e73
machonky <martin@macrobert.com>
2024-04-22 18:13:23 +0800

    BusinessDayAppService

---
+++ src/AbpFullCalendar.Application.Contracts/IBusinessDayAppService.cs
@@ -0,0 +1,14 @@
+﻿using AbpFullCalendar.BusinessDays;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace AbpFullCalendar;
+
+public interface IBusinessDayAppService : IApplicationService
+{
+    Task<IList<BusinessDayEventDto>> GetBusinessDaysAsync(DateTime start, DateTime end);
+    Task StoreBusinessDaysAsync(SelectedBusinessDayEventsDto selectedBusinessDays);
+}
--- src/AbpFullCalendar.Domain/BusinessDays/BusinessDayKey.cs
+++ src/AbpFullCalendar.Domain/BusinessDays/BusinessDayKey.cs
@@ -1,6 +1,34 @@
 ﻿
 using MacRobert.StronglyTypeIds;
+using System;
 
 namespace AbpFullCalendar.BusinessDays;
 
-public record BusinessDayKey(int Value) : IStronglyTypedId<int>;
+public record BusinessDayKey(int Value) : IStronglyTypedId<int>
+{
+    public static bool operator <(BusinessDayKey a, BusinessDayKey b)
+    {
+        return a.Value < b.Value;
+    }
+
+    public static bool operator >(BusinessDayKey a, BusinessDayKey b)
+    {
+        return a.Value > b.Value;
+    }
+
+    public static bool operator <=(BusinessDayKey a, BusinessDayKey b)
+    {
+        return a < b || a == b;
+    }
+
+    public static bool operator >=(BusinessDayKey a, BusinessDayKey b)
+    {
+        return a > b || a == b;
+    }
+}
+
+public static class BusinessDayKeyExtensions
+{
+    public static BusinessDayKey ToDateKey(this DateTime date) => new BusinessDayKey(int.Parse(date.ToString("yyyyMMdd")));
+    public static DateOnly FromDateKey(this BusinessDayKey dateIndex) => new DateOnly(year: dateIndex.Value / 10000, month: (dateIndex.Value / 100) % 100, day: dateIndex.Value % 100);
+}

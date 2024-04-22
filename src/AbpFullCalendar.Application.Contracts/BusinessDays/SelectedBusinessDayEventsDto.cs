28efe3502c80e45c9c72d2e01d9b7bc817282f88
machonky <martin@macrobert.com>
2024-04-22 18:12:08 +0800

    netstandard2 compatibility

--- src/AbpFullCalendar.Application.Contracts/BusinessDays/SelectedBusinessDayEventsDto.cs
+++ src/AbpFullCalendar.Application.Contracts/BusinessDays/SelectedBusinessDayEventsDto.cs
@@ -1,7 +1,9 @@
-﻿namespace AbpFullCalendar.BusinessDays;
+﻿using System;
+
+namespace AbpFullCalendar.BusinessDays;
 
 public class SelectedBusinessDayEventsDto
 {
-    public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
 }

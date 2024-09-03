0031c271055b42af25f6ed05467126ea68743494
machonky <martin@macrobert.com>
2024-09-03 14:18:33 +0800

    Correctly package fullcalendar.io + upgrade to 6.1.15

--- src/AbpFullCalendar.Web/abp.resourcemapping.js
+++ src/AbpFullCalendar.Web/abp.resourcemapping.js
@@ -1,11 +1,12 @@
 module.exports = {
     aliases: {
-
+        "@node_modules": "./node_modules",
+        "@libs": "./wwwroot/libs"
     },
     clean: [
-
+        "@libs"
     ],
     mappings: {
-
+        "@node_modules/fullcalendar/index.global.min.js": "@libs/fullcalendar"
     }
 };
